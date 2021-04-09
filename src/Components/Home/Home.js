79223bef1bd3f45d56065d021421f78445337366
mdmonsuralamrana <monsuralam7262@gmail.com>
2021-04-09 19:31:13 +0600

    spinner added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
+import LoadingSpinner from '../../icons/Loading.gif'
 import './Home.css';
 
 const Home = () => {
-    const [products , setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         fetch('https://islamia-store.herokuapp.com/products')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
 
     return (
         <div className="container home mt-5">
+            {
+                products.length === 0 && <img className="spinner" src={LoadingSpinner} alt=""></img>
+            }
             {
                 products.map(product => <Product key={product._id} product={product}></Product>)
             }
